431579ff279fcf40d21219688a8e8c0b7171af28
tf2164 <101139841+tf2164@users.noreply.github.com>
2022-05-02 16:37:44 -0400

    Added first problem

---
+++ Easy-1.js
@@ -0,0 +1,15 @@
+//Easy1
+let easyArray = [2, 4, 7, 11, 15, 16];
+
+
+    
+let odds = easyArray.filter(n => n%2);
+
+console.log(`Odd numbers: ${odds}`);
+
+
+let evens = easyArray.filter((n) => n % 2 === 0 );
+
+console.log(`Even numbers: ${evens}`);
+
+
