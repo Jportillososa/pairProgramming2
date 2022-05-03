7173f100b09ea39ec79ce2742d2d7b69f7a0f263
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-05-03 16:36:31 -0400

    adding Medium-4.js

---
+++ Medium-4.js
@@ -0,0 +1,27 @@
+// Create a car object with the items: Make, Model, Year, Milage, and Color. Then create 3 methods in the object;
+// A driveToWork method, driveAroundTheWorld method, and runErrands method. Each of these methods 
+// should affect the car’s mileage adding to it each time and console logging the old mileage and the new 
+// mileage.
+
+//NAVIGATOR: JOSE PORTILLO
+//DRIVER: TYRA FAIR
+
+
+
+let car = {
+    make: "Ford",
+    model: "Ranger",
+    year: 2021,
+    mileage: 123456,
+    color: "black"
+}
+
+let driveToWork = () => (car.mileage + 33);
+let driveAroundTheWorld = () => (car.mileage + 24000);
+let runErrands = () => (car.mileage + 30);
+
+
+console.log(`This is the old mileage : ${car.mileage}`);
+console.log(`This is the new mileage after going to work : ${driveToWork()}`);
+console.log(`This is the new mileage after driving around the world : ${driveAroundTheWorld()}`);
+console.log(`This is the new mileage after running Errands : ${runErrands()}`);
