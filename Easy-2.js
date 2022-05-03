2f853215659112de0882b38769e9891c21c370b0
Jose Portillo <jportillososa@MacBook-Pro.attlocal.net>
2022-05-03 14:12:45 -0400

    adding file

---
+++ Easy-2.js
@@ -0,0 +1,26 @@
+function numArray(arr) {
+    let sortArray = [];
+    arr.forEach((num) => {
+        // FOR EACH RUNS EACH NUMBERS IN ARRAY AND EXECUTES A FUNCTION
+        // (NUM)IS PARAMETER FOR THE NUMBERS
+        let factor = 0; //another variable which starts at 0 everytime you run through number
+        for (let i = 0; i <= num; i++) {
+            //FOR LOOP IS CREATED (starts at zero; i is less than or = to num; loops every integer)
+            //( if remainder (num %i) is = 0 it means that i is a factor of num )
+            if ((num % i) === 0) {
+                factor++  //everytime you find a factor this continues the loop
+            }
+        }
+        if (factor === 2) {
+            // if factor is = 2 this number is an array-then it pushes to the array
+            sortArray.push(num)
+        }
+    })
+
+    console.log(`Array of Prime Numbers : [${sortArray}]`);
+
+
+    //logs new array
+}
+
+numArray([10, 18, 19, 29, 33, 35, 47, 66, 83])
